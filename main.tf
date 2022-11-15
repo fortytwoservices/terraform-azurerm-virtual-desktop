7699a75bc85133a1f09ba440cc6714155642a0eb
Sindre Grindvoll <sindre.grindvoll@gmail.com>
2022-11-15 09:23:45 +0100

    fixed typo in for_each resource block reference

--- main.tf
+++ main.tf
@@ -41,7 +41,7 @@ resource "azurerm_virtual_desktop_host_pool" "avd-host_pools" {
   tags                             = lookup(each.value, "tags", null) != null ? each.value.tags : local.tags
 
   dynamic "scheduled_agent_updates" {
-    for_each = { for scheduled in each.value.scheduled_agent_updates : scheduled.timezone => schedule }
+    for_each = { for scheduled in each.value.scheduled_agent_updates : scheduled.timezone => scheduled }
     iterator = scheduled
 
     content {
