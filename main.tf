5413c86221f026c3ebf93b3c3e7f8636c232f0d2
Sindre Grindvoll <sindre.grindvoll@gmail.com>
2022-11-16 11:01:47 +0100

    fixed input of scheduled_agent_updates

--- main.tf
+++ main.tf
@@ -49,7 +49,7 @@ resource "azurerm_virtual_desktop_host_pool" "avd-host_pools" {
       use_session_host_timezone = lookup(each.value.scheduled_agent_updates, "use_session_host_timezone", null)
 
       dynamic "schedule" {
-        for_each = { for schedule in each.value.scheduled_agent_updates : schedule.day_of_week => schedule }
+        for_each = { for schedule in each.value.scheduled_agent_updates.schedule : schedule.day_of_week => schedule }
 
         content {
           day_of_week = lookup(schedule.value, "day_of_week", null)
