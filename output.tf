eaff0eb5f9c4a6b441c3ca506ccbcb2f858ac9cb
Sindre Grindvoll <sindre.grindvoll@gmail.com>
2023-01-11 10:57:00 +0100

    added outputs from all resources

--- output.tf
+++ output.tf
@@ -1,3 +1,27 @@
 ###############
 ##  Outputs  ##
 ###############
+output "avd-host_pools" {
+  value = azurerm_virtual_desktop_host_pool.avd-host_pools[*]
+}
+
+output "avd-host_pool_registrations" {
+  value = azurerm_virtual_desktop_host_pool_registration_info.avd-host_pool_registrations[*]
+}
+
+output "avd-application_groups" {
+  value = azurerm_virtual_desktop_application_group.avd-application_groups[*]
+}
+
+output "avd-applications" {
+  value = azurerm_virtual_desktop_application.avd-applications[*]
+}
+
+output "avd-shared_image_galleries" {
+  value = azurerm_shared_image_gallery.avd-shared_image_galleries[*]
+}
+
+output "avd-session-hosts" {
+  value = azurerm_windows_virtual_machine.avd-session-hosts[*]
+}
+
