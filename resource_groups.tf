0dece22a5a44e10f18d7479c57ca358abdbe9711
Sindre Grindvoll <sindre.grindvoll@gmail.com>
2023-05-12 10:01:09 +0200

    feat[terraform]: updated default tags behaviour to check if var.tags is populated
    Signed-off-by: Sindre Grindvoll <sindre.grindvoll@gmail.com>

--- resource_groups.tf
+++ resource_groups.tf
@@ -5,25 +5,25 @@ resource "azurerm_resource_group" "avd" {
   for_each = { for workspace in var.avd-workspaces : workspace.name => workspace }
   name     = "${local.prefix}-${each.key}"
   location = each.value.location != null ? each.value.location : var.location
-  tags     = each.value.tags != null ? each.value.tags : local.tags
+  tags     = each.value.tags != null ? each.value.tags : ( var.tags != null ? var.tags : local.tags )
 }
 
 resource "azurerm_resource_group" "avd-session_hosts" {
   for_each = local.session_host_vms
   name     = "${local.prefix}-${each.key}"
   location = var.location
-  tags     = each.value.tags != null ? each.value.tags : local.tags
+  tags     = each.value.tags != null ? each.value.tags : ( var.tags != null ? var.tags : local.tags )
 }
 
 resource "azurerm_resource_group" "avd-fslogix" {
   name     = "${local.prefix}-avd-fslogix"
   location = var.location
-  tags     = local.tags
+  tags     = var.tags != null ? var.tags : local.tags
 }
 
 resource "azurerm_resource_group" "avd-shared_image_galleries" {
   for_each = { for sig in var.avd-shared-image-gallery : sig.name => sig }
   name     = "${local.prefix}-${each.key}"
   location = var.location
-  tags     = local.tags
+  tags     = var.tags != null ? var.tags : local.tags
 }
