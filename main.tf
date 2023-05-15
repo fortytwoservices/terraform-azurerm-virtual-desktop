316be9baf11018b2a6c66da3d5e518c3c741f30e
Sindre Grindvoll <sindre.grindvoll@gmail.com>
2023-05-15 15:23:07 +0200

    feat[terraform]: added dependencies for role assignments before fslogix file share is created
    Signed-off-by: Sindre Grindvoll <sindre.grindvoll@gmail.com>

--- main.tf
+++ main.tf
@@ -159,7 +159,7 @@ resource "azurerm_storage_account" "avd-fslogix" {
   access_tier              = each.value.access_tier
 
   dynamic "azure_files_authentication" {
-    for_each = each.value.azure_domain_join_type != null? [1] : []
+    for_each = each.value.azure_domain_join_type != null ? [1] : []
 
     content {
       directory_type = each.value.azure_domain_join_type
@@ -168,14 +168,14 @@ resource "azurerm_storage_account" "avd-fslogix" {
 }
 
 resource "azurerm_role_assignment" "avd-fslogix-smb-share-contributor-tf-deployment-spn" {
-  for_each = { for sa in var.avd-fslogix : sa.name => sa if sa.azure_domain_join_type != null ? true : false }
+  for_each             = { for sa in var.avd-fslogix : sa.name => sa if sa.azure_domain_join_type != null ? true : false }
   principal_id         = each.value.terraform_deployment_spn_object_id
   scope                = azurerm_storage_account.avd-fslogix[each.key].id
   role_definition_name = "Storage File Data SMB Share Contributor"
 }
 
 resource "azurerm_role_assignment" "avd-fslogix-smb-share-contributor-avd-users" {
-  for_each = { for sa in var.avd-fslogix : sa.name => sa if sa.azure_domain_join_type != null ? true : false }
+  for_each             = { for sa in var.avd-fslogix : sa.name => sa if sa.azure_domain_join_type != null ? true : false }
   principal_id         = each.value.ad_group_avd_users_object_id
   scope                = azurerm_storage_account.avd-fslogix[each.key].id
   role_definition_name = "Storage File Data SMB Share Contributor"
@@ -196,6 +196,11 @@ resource "azurerm_storage_share" "avd-fslogix-file-share" {
       quota
     ]
   }
+
+  depends_on = [
+    azurerm_role_assignment.avd-fslogix-smb-share-contributor-avd-users,
+    azurerm_role_assignment.avd-fslogix-smb-share-contributor-tf-deployment-spn
+  ]
 }
 
 resource "azurerm_storage_share_directory" "avd-fslogix-file-share-directory" {
