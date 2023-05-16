6661ce68d0d725f036900423d633113c2171f252
Sindre Grindvoll <sindre.grindvoll@gmail.com>
2023-05-16 08:56:57 +0200

    feat[azurerm]: added role assignment for virtual machine user login on resource group containing the session hosts. also added dedicated input to it
    Signed-off-by: Sindre Grindvoll <sindre.grindvoll@gmail.com>

--- resource_groups.tf
+++ resource_groups.tf
@@ -5,20 +5,20 @@ resource "azurerm_resource_group" "avd" {
   for_each = { for workspace in var.avd-workspaces : workspace.name => workspace }
   name     = "${local.prefix}-${each.key}"
   location = each.value.location != null ? each.value.location : var.location
-  tags     = each.value.tags != null ? each.value.tags : ( var.tags != null ? var.tags : local.tags )
+  tags     = each.value.tags != null ? each.value.tags : (var.tags != null ? var.tags : local.tags)
 }
 
 resource "azurerm_resource_group" "avd-session_hosts" {
   for_each = local.session_host_vms
   name     = "${local.prefix}-${each.key}"
   location = var.location
-  tags     = each.value.tags != null ? each.value.tags : ( var.tags != null ? var.tags : local.tags )
+  tags     = each.value.tags != null ? each.value.tags : (var.tags != null ? var.tags : local.tags)
 }
 
 resource "azurerm_role_assignment" "avd-virtual-machine-user-login" {
-  for_each = local.session_host_vms
-  scope = azurerm_resource_group.avd-session_hosts[each.key].id
-  principal_id = each.value.group-avd-users
+  for_each             = local.session_host_vms
+  scope                = azurerm_resource_group.avd-session_hosts[each.key].id
+  principal_id         = each.value.group-avd-users
   role_definition_name = "Virtual Machine User Login"
 }
 
