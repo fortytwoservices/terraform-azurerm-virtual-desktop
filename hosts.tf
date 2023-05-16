5a14f8dd8d55ca57ad07ea4b5c6bad5855656ca2
Sindre Grindvoll <sindre.grindvoll@gmail.com>
2023-05-16 08:38:52 +0200

    feat[terraform]: fixed tags for all resources, ensuring that they are always populated in the correct order. also upped the required minimum version
    Signed-off-by: Sindre Grindvoll <sindre.grindvoll@gmail.com>

--- hosts.tf
+++ hosts.tf
@@ -22,9 +22,9 @@ resource "azurerm_availability_set" "avd-session-host-availability_sets" {
   name                         = "${local.prefix}-${each.key}-availability-set"
   resource_group_name          = azurerm_resource_group.avd-session_hosts[each.key].name
   location                     = azurerm_resource_group.avd-session_hosts[each.key].location
-  platform_update_domain_count = lookup(each.value, "platform_update_domain_count", null)
-  platform_fault_domain_count  = lookup(each.value, "platform_fault_domain_count", null)
-  tags                         = lookup(each.value, "tags", local.tags)
+  platform_update_domain_count = try(each.value.platform_update_domain_count, null)
+  platform_fault_domain_count  = try(each.value.platform_fault_domain_count, null)
+  tags                         = each.value.tags != null ? each.value.tags : (var.tags != null ? var.tags : local.tags)
 }
 
 
@@ -36,7 +36,7 @@ resource "azurerm_storage_account" "avd-session-host-sa-boot_diagnostics" {
   location                 = azurerm_resource_group.avd-session_hosts[each.key].location
   account_tier             = "Standard"
   account_replication_type = "LRS"
-  tags                     = lookup(each.value, "tags", local.tags)
+  tags                     = each.value.tags != null ? each.value.tags : (var.tags != null ? var.tags : local.tags)
 }
 
 
@@ -55,7 +55,7 @@ resource "azurerm_network_interface" "avd-session-host-nics" {
     subnet_id                     = each.value.subnet_id
   }
 
-  tags = lookup(each.value, "tags", local.tags)
+  tags = each.value.tags != null ? each.value.tags : (var.tags != null ? var.tags : local.tags)
 }
 
 
@@ -108,7 +108,7 @@ resource "azurerm_windows_virtual_machine" "avd-session-hosts" {
     storage_account_uri = azurerm_storage_account.avd-session-host-sa-boot_diagnostics[each.key].primary_blob_endpoint
   }
 
-  tags = lookup(each.value, "tags", local.tags)
+  tags = each.value.tags != null ? each.value.tags : (var.tags != null ? var.tags : local.tags)
 
   lifecycle {
     ignore_changes = [
@@ -151,6 +151,7 @@ resource "azurerm_virtual_machine_extension" "avd-session-host-azuread-join" {
   type                       = "AADLoginForWindows"
   type_handler_version       = "2.0"
   auto_upgrade_minor_version = true
+  tags                       = azurerm_windows_virtual_machine.avd-session-hosts[each.value.host_key].tags
 }
 
 ### Conditional deployment of Azure Active Directory Domain Services join
@@ -163,6 +164,7 @@ resource "azurerm_virtual_machine_extension" "avd-session-host-aadds-join" {
   type                       = "JsonADDomainExtension"
   type_handler_version       = "1.3"
   auto_upgrade_minor_version = true
+  tags                       = azurerm_windows_virtual_machine.avd-session-hosts[each.value.host_key].tags
 
   settings = <<-SETTINGS
     {
@@ -193,6 +195,7 @@ resource "azurerm_virtual_machine_extension" "avd-session-host-registration" {
   publisher            = "Microsoft.Powershell"
   type                 = "DSC"
   type_handler_version = "2.73"
+  tags                 = azurerm_windows_virtual_machine.avd-session-hosts[each.value.host_key].tags
 
   settings = <<-SETTINGS
     {
