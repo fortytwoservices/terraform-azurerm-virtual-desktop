da80c8fbbcf107d691097bfbddfb7089f78bba94
Ketil Gjerde <ketil.gjerde@fortytwo.io>
2024-07-30 13:06:35 +0200

    docs: update example version
    Signed-off-by: Ketil Gjerde <ketil.gjerde@fortytwo.io>

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -4,7 +4,7 @@
 
 module "avd1" {
   source  = "fortytwoservices/virtual-desktop/azurerm"
-  version = "2.0.1"
+  version = "2.1.0"
 
   shortname = local.shortname    # Shortname appended to the beginning of all resources. Ommit this to not append this prefix to all resource names. Eg: Fortytwo would be ft
   env       = "dev"              # What environment the resources are deployed in. Expected values: p, prod, d, dev, t, test, q, qa, s, stage
